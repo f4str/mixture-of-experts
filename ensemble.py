ce85721ed335b7c9df19e50f0d796d9939dcc3c5
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-04-15 14:12:19 -0400

    implemented first mixture of experts

---
+++ ensemble.py
@@ -0,0 +1,83 @@
+"""
+Ensemble of Convolutional Neural Networks
+MNIST Classifier
+Average all Logits
+Train ensemble
+"""
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from mnist_networks import LeNet
+
+
+class Ensemble:
+	def __init__(self, num_ensembles=3):
+		self.sess = tf.Session()
+		
+		self.learning_rate = 0.001
+		self.batch_size = 64
+		
+		self.num_inputs = 784
+		self.num_classes = 10
+		self.num_ensembles = num_ensembles
+		
+		self.load_data()
+		self.build()
+	
+	def load_data(self):
+		mnist = input_data.read_data_sets('data/MNIST/', one_hot=True)
+		self.training_data = mnist.train
+		self.X_valid = mnist.validation.images
+		self.y_valid = mnist.validation.labels
+		self.X_test = mnist.test.images
+		self.y_test = mnist.test.labels
+	
+	def build(self):
+		with tf.variable_scope('ensemble', reuse=tf.AUTO_REUSE) as scope:
+			self.x = tf.placeholder(tf.float32, [None, self.num_inputs], name='x')
+			self.y = tf.placeholder(tf.float32, [None, self.num_classes], name='y')
+		
+		self.networks = [LeNet() for _ in range(self.num_ensembles)]
+		
+		stack = tf.stack([net.logits for net in self.networks], axis=-1)
+		self.logits = tf.reduce_mean(stack, axis=-1)
+		
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y)
+		self.loss = tf.reduce_mean(cross_entropy)
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+		
+		self.prediction = tf.argmax(self.logits, axis=1)
+		correct_prediction = tf.equal(self.prediction, tf.argmax(self.y, axis=1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		self.predict = tf.nn.softmax(self.logits)
+		
+	def train(self, epochs=500):
+		self.sess.run(tf.global_variables_initializer())
+		
+		for e in range(epochs):
+			x_batch, y_batch = self.training_data.next_batch(self.batch_size)
+			feed_dict = {self.x: x_batch, self.y: y_batch}
+			self.sess.run(self.optimizer, feed_dict=feed_dict)
+			train_loss, train_acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
+			
+			feed_dict = {self.x: self.X_valid, self.y: self.y_valid}
+			valid_loss, valid_acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
+			
+			print(f'epoch {e + 1}:',
+				f'train loss = {train_loss:.4f},',
+				f'train acc = {train_acc:.4f},',
+				f'valid loss = {valid_loss:.4f},',
+				f'valid acc = {valid_acc:.4f}'
+			)
+		
+		print('training complete')
+		
+		feed_dict = {self.x: self.X_test, self.y: self.y_test}
+		
+		loss, acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
+		print(f'test loss = {loss:.4f}, test acc = {acc:.4f}')
+
+
+if __name__ == '__main__':
+	model = Ensemble()
+	model.train(20)
---
+++ mnist_networks.py
@@ -0,0 +1,188 @@
+"""
+Convolutional Neural Networks
+MNIST classifiers
+"""
+
+import tensorflow as tf
+
+
+def conv2d_layer(inputs, filters, kernel_size=5, strides=1, padding='SAME'):
+	channels = inputs.get_shape()[-1]
+	weights = tf.Variable(tf.random.truncated_normal(
+		shape=[kernel_size, kernel_size, channels, filters],
+		mean=0,
+		stddev=0.1
+	))
+	biases = tf.Variable(tf.zeros([filters]))
+	layer = tf.nn.conv2d(
+		inputs,
+		filter=weights,
+		strides=[1, strides, strides, 1],
+		padding=padding
+	) + biases
+	return tf.nn.relu(layer)
+
+
+def maxpool2d_layer(inputs, k=2, padding='SAME'):
+	return tf.nn.max_pool2d(
+		inputs,
+		ksize=[1, k, k, 1],
+		strides=[1, k, k, 1],
+		padding=padding
+	)
+
+
+def flatten_layer(layer):
+	layer_shape = layer.get_shape()
+	num_features = layer_shape[1:4].num_elements()
+	return tf.reshape(layer, [-1, num_features])
+
+
+def linear_layer(inputs, num_outputs, relu=True):
+	num_inputs = inputs.get_shape()[-1]
+	weights = tf.Variable(tf.random.truncated_normal(
+		shape=[num_inputs, num_outputs],
+		mean=0,
+		stddev=0.1
+	))
+	biases = tf.Variable(tf.zeros([num_outputs]))
+	layer = tf.matmul(inputs, weights) + biases
+	if relu:
+		return tf.nn.relu(layer)
+	else:
+		return layer
+
+
+class FeedForward:
+	def __init__(self):
+		x = tf.get_default_graph().get_tensor_by_name('ensemble/x:0')
+		y = tf.get_default_graph().get_tensor_by_name('ensemble/y:0')
+		
+		# Layer 1 = Linear: 784 -> 512
+		linear1 = linear_layer(x, num_outputs=512)
+		# Layer 2 = Linear: 512 -> 256
+		linear2 = linear_layer(linear1, num_outputs=256)
+		# Layer 3 = Linear: 256 -> 64
+		linear3 = linear_layer(linear2, num_outputs=64)
+		# Layer 4 = Linear: 64 -> 10
+		self.logits = linear_layer(linear3, num_outputs=10)
+		
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y)
+		self.loss = tf.reduce_mean(cross_entropy)
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+		
+		self.prediction = tf.argmax(self.logits, axis=1)
+		correct_prediction = tf.equal(self.prediction, tf.argmax(y, axis=1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		self.predict = tf.nn.softmax(self.logits)
+
+
+class LeNet:
+	def __init__(self):
+		x = tf.get_default_graph().get_tensor_by_name('ensemble/x:0')
+		y = tf.get_default_graph().get_tensor_by_name('ensemble/y:0')
+		
+		# Layer 0 = Reshape: 784 -> 28x28@1
+		x_img = tf.reshape(x, shape=[-1, 28, 28, 1])
+		# Layer 1 = Convolution: 28x28@1 -> 28x28@32 + ReLU
+		conv1 = conv2d_layer(x_img, filters=32, kernel_size=5, padding='SAME')
+		# Layer 2 = Pooling: 28x28@32 -> 14x14@32
+		pool1 = maxpool2d_layer(conv1, padding='SAME')
+		# Layer 3 = Convolution: 14x14@32 -> 14x14@64 + ReLU
+		conv2 = conv2d_layer(pool1, filters=64, kernel_size=5, padding='SAME')
+		# Layer 4 = Pooling: 14x14@64 -> 7x7@64
+		pool2 = maxpool2d_layer(conv2, padding='SAME')
+		# Layer 5 = Flatten: 7x7@64 -> 3136
+		flat = flatten_layer(pool2)
+		# Layer 6 = Fully Connected: 3136 -> 512
+		fc1 = linear_layer(flat, num_outputs=512)
+		# Layer 7 = Fully Connected: 512 -> 128
+		fc2 = linear_layer(fc1, num_outputs=128)
+		# Layer 8 = Logits: 128 -> 10
+		self.logits = linear_layer(fc2, num_outputs=10, relu=False)
+		
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y)
+		self.loss = tf.reduce_mean(cross_entropy)
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+		
+		self.prediction = tf.argmax(self.logits, axis=1)
+		correct_prediction = tf.equal(self.prediction, tf.argmax(y, axis=1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		self.predict = tf.nn.softmax(self.logits)
+
+
+class AlexNet:
+	def __init__(self):
+		x = tf.get_default_graph().get_tensor_by_name('ensemble/x:0')
+		y = tf.get_default_graph().get_tensor_by_name('ensemble/y:0')
+		
+		# Layer 0 = Reshape: 784 -> 28x28@1
+		x_img = tf.reshape(x, shape=[-1, 28, 28, 1])
+		# Layer 1 = Convolution: 28x28@1 -> 28x28@32 + ReLU
+		conv1 = conv2d_layer(x_img, filters=32, kernel_size=3, padding='SAME')
+		# Layer 2 = Pooling: 28x28@32 -> 14x14@32
+		pool1 = maxpool2d_layer(conv1, padding='SAME')
+		# Layer 3 = Convolution: 14x14@32 -> 14x14@64 + ReLU
+		conv2 = conv2d_layer(pool1, filters=64, kernel_size=3, padding='SAME')
+		# Layer 4 = Pooling: 14x14@64 -> 7x7@64
+		pool2 = maxpool2d_layer(conv2, padding='SAME')
+		# Layer 5 = Convolution: 7x7@64 -> 7x7@128 + ReLU
+		conv3 = conv2d_layer(pool2, filters=128, kernel_size=3, padding='SAME')
+		# Layer 6 = Pooling: 7x7@128 -> 4x4@128
+		pool3 = maxpool2d_layer(conv3, padding='SAME')
+		# Layer 7 = Flatten: 4x4@128 -> 2048
+		flat = flatten_layer(pool3)
+		# Layer 8 = Fully Connected: 2048 -> 512
+		fc1 = linear_layer(flat, num_outputs=512)
+		# Layer 9 = Fully Connected: 1024 -> 128
+		fc2 = linear_layer(fc1, num_outputs=128)
+		# Layer 10 = Logits: 128 -> 10
+		self.logits = linear_layer(fc2, num_outputs=10, relu=False)
+		
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y)
+		self.loss = tf.reduce_mean(cross_entropy)
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+		
+		self.prediction = tf.argmax(self.logits, axis=1)
+		correct_prediction = tf.equal(self.prediction, tf.argmax(y, axis=1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		self.predict = tf.nn.softmax(self.logits)
+
+
+class VGGNet:
+	def __init__(self):
+		x = tf.get_default_graph().get_tensor_by_name('ensemble/x:0')
+		y = tf.get_default_graph().get_tensor_by_name('ensemble/y:0')
+		
+		# Layer 0 = Reshape: 784 -> 28x28@1
+		x_img = tf.reshape(x, shape=[-1, 28, 28, 1])
+		# Layer 1 = Convolution: 28x28@1 -> 28x28@32 + ReLU (x2)
+		conv1_1 = conv2d_layer(x_img, filters=32, kernel_size=3, padding='SAME')
+		conv1_2 = conv2d_layer(conv1_1, filters=32, kernel_size=3, padding='SAME')
+		# Layer 2 = Pooling: 28x28@32 -> 14x14@32
+		pool1 = maxpool2d_layer(conv1_2, padding='SAME')
+		# Layer 3 = Convolution: 14x14@32 -> 14x14@64 + ReLU (x2)
+		conv2_1 = conv2d_layer(pool1, filters=64, kernel_size=3, padding='SAME')
+		conv2_2 = conv2d_layer(conv2_1, filters=64, kernel_size=3, padding='SAME')
+		# Layer 4 = Pooling: 14x14@64 -> 7x7@64
+		pool2 = maxpool2d_layer(conv2_2, padding='SAME')
+		# Layer 5 = Flatten: 7x7@64 -> 3136
+		flat = flatten_layer(pool2)
+		# Layer 6 = Fully Connected: 3136 -> 512
+		fc1 = linear_layer(flat, num_outputs=512)
+		# Layer 7 = Fully Connected: 512 -> 128
+		fc2 = linear_layer(fc1, num_outputs=128)
+		# Layer 8 = Logits: 128 -> 10
+		self.logits = linear_layer(fc2, num_outputs=10, relu=False)
+		
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y)
+		self.loss = tf.reduce_mean(cross_entropy)
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+		
+		self.prediction = tf.argmax(self.logits, axis=1)
+		correct_prediction = tf.equal(self.prediction, tf.argmax(y, axis=1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		self.predict = tf.nn.softmax(self.logits)
+
+
+
